b3011cfaca9369649bbf776dfcd0b41a09350b61
printfjoby <printfjoby@gmail.com>
2021-06-07 13:19:03 +0530

    modified navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import logo from '../p-logo.jpg'
 
-import { Container, Row, Col } from 'bootstrap/dist/css/bootstrap.css'
 class Navbar extends Component {
   render() {
     return (
